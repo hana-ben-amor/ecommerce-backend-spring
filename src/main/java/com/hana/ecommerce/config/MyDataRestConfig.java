bbba76a794e69b424f4537e5cc1a564f313bf8c2
Hanabenamor <hanaamor5@gmail.com>
2026-03-11 11:13:26 +0100

    Fixing Cors issue

---
+++ src/main/java/com/hana/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,24 @@
+package com.hana.ecommerce.config;
+
+import com.hana.ecommerce.entities.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        // Disable POST, PUT, DELETE for Product (read-only)
+        HttpMethod[] unsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
---
+++ src/main/java/com/hana/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.hana.ecommerce.repositories;
+
+import com.hana.ecommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
